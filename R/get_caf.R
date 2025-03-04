81555c1e0368b481dbc541c363e775777c3dc7df
Jim Grange <grange.jim@gmail.com>
2025-03-04 10:49:58 +0000

    added caf function

---
+++ R/get_caf.R
@@ -0,0 +1,200 @@
+get_caf <- function(data,
+                    rt_var = "rt",
+                    accuracy_var = "accuracy",
+                    id_var = NULL,
+                    conditions = NULL,
+                    n_bins = 4){
+
+  # if id is null (i.e., perhaps it's data from just a single participant or
+  # a model), add an id column
+  if(is.null(id_var)){
+    data <- data |>
+      mutate(id = 1)
+  }
+
+  # if id is not null, add column called id
+  if(!is.null(id_var)){
+    if(id_var != "id"){
+      data$id <- data[[id_var]]
+    }
+  }
+
+
+  # get the CAF values for each individual
+  id_data <- data |>
+    group_by(.data$id,
+             across(all_of(conditions))) |>
+    mutate(rt_bin = ntile(.data$rt, n_bins)) |>
+    group_by(.data$id,
+             across(all_of(conditions)),
+             rt_bin) |>
+    summarise(
+      m_rt = mean(.data$rt),
+      m_accuracy = mean(.data$accuracy),
+      .groups = "drop"
+    )
+
+  # get the CAF data averaged across subjects
+  if(is.null(id_var)){
+    id_averaged_data <- id_data |>
+      rename(mean_rt = .data$m_rt,
+             mean_accuracy = .data$m_accuracy) |>
+      mutate(se_rt = 0,
+             se_acc = 0)
+  } else {
+    id_averaged_data <- id_data |>
+      group_by(across(all_of(conditions)),
+               .data$rt_bin) |>
+      summarise(mean_rt = mean(.data$m_rt),
+                se_rt = sd(.data$m_rt) / sqrt(length(.data$m_rt)),
+                mean_acc = mean(.data$m_accuracy),
+                se_acc = sd(.data$m_accuracy) /
+                  sqrt(length(.data$m_accuracy)),
+                .groups = "drop")
+  }
+
+  if(id_var != "id"){
+    data <- data |>
+      select(-.data$id)
+  }
+
+
+  #--- do the plotting
+
+  # plot if there are no conditions
+  if(is.null(conditions)){
+    plot <- id_averaged_data |>
+      ggplot(aes(x = .data$mean_rt,
+                 y = .data$mean_acc)) +
+      geom_point(size = 3) +
+      geom_line(linewidth = 1) +
+      geom_errorbar(aes(ymin = .data$mean_acc - .data$se_acc,
+                        ymax = .data$mean_acc + .data$se_acc),
+                    width = 0.0) +
+      geom_errorbarh(aes(xmin = .data$mean_rt - .data$se_rt,
+                         xmax = .data$mean_rt + .data$se_rt),
+                     height = 0.0) +
+      theme_bw() +
+      labs(x = "Mean Response Time (ms)",
+           y = "Proportion Accuracy") +
+      theme(text = element_text(size = 18)) +
+      theme(legend.position = "bottom")
+  }
+
+  # plot if there is one condition
+  if(length(conditions) == 1){
+    id_averaged_data$condition <- as.factor(id_averaged_data[[conditions]])
+
+    plot <- id_averaged_data |>
+      ggplot(aes(x = .data$mean_rt,
+                 y = .data$mean_acc,
+                 group = .data$condition)) +
+      geom_point(size = 3,
+                 aes(colour = .data$condition)) +
+      geom_line(aes(colour = .data$condition),
+                linewidth = 1) +
+      geom_errorbar(aes(colour = .data$condition,
+                        ymin = .data$mean_acc - .data$se_acc,
+                        ymax = .data$mean_acc + .data$se_acc),
+                    width = 0.0) +
+      geom_errorbarh(aes(colour = .data$condition,
+                         xmin = .data$mean_rt - .data$se_rt,
+                         xmax = .data$mean_rt + .data$se_rt),
+                     height = 0.0) +
+      theme_bw() +
+      scale_colour_discrete(name = conditions) +
+      labs(x = "Mean Response Time (ms)",
+           y = "Proportion Accuracy") +
+      theme(text = element_text(size = 18)) +
+      theme(legend.position = "bottom")
+
+    # remove the conditions tag
+    id_averaged_data <- id_averaged_data |> select(-.data$condition)
+
+  }
+
+  # plot if there are two conditions
+  if(length(conditions) == 2){
+    id_averaged_data$condition_1 <- as.factor(id_averaged_data[[conditions[1]]])
+    id_averaged_data$condition_2 <- as.factor(id_averaged_data[[conditions[2]]])
+
+    plot <- id_averaged_data |>
+      ggplot(aes(x = .data$mean_rt,
+                 y = .data$mean_acc,
+                 group = .data$condition_1)) +
+      geom_point(size = 3,
+                 aes(colour = .data$condition_1)) +
+      geom_line(aes(colour = .data$condition_1),
+                linewidth = 1) +
+      geom_errorbar(aes(colour = .data$condition_1,
+                        ymin = .data$mean_acc - .data$se_acc,
+                        ymax = .data$mean_acc + .data$se_acc),
+                    width = 0.0) +
+      geom_errorbarh(aes(colour = .data$condition_1,
+                         xmin = .data$mean_rt - .data$se_rt,
+                         xmax = .data$mean_rt + .data$se_rt),
+                     height = 0.0) +
+      theme_bw() +
+      facet_grid(.~.data$condition_2) +
+      scale_colour_discrete(name = conditions) +
+      labs(x = "Mean Response Time (ms)",
+           y = "Proportion Accuracy") +
+      theme(text = element_text(size = 18)) +
+      theme(legend.position = "bottom")
+  }
+
+
+
+  # plot if there are three conditions
+  if(length(conditions) == 3){
+
+    id_averaged_data$condition_1 <- as.factor(id_averaged_data[[conditions[1]]])
+    id_averaged_data$condition_2 <- as.factor(id_averaged_data[[conditions[2]]])
+    id_averaged_data$condition_3 <- as.factor(id_averaged_data[[conditions[3]]])
+
+    plot <- id_averaged_data |>
+      ggplot(aes(x = .data$mean_rt,
+                 y = .data$mean_acc,
+                 group = .data$condition_1)) +
+      geom_point(size = 3,
+                 aes(colour = .data$condition_1)) +
+      geom_line(aes(colour = .data$condition_1),
+                linewidth = 1) +
+      geom_errorbar(aes(colour = .data$condition_1,
+                        ymin = .data$mean_acc - .data$se_acc,
+                        ymax = .data$mean_acc + .data$se_acc),
+                    width = 0.0) +
+      geom_errorbarh(aes(colour = .data$condition_1,
+                         xmin = .data$mean_rt - .data$se_rt,
+                         xmax = .data$mean_rt + .data$se_rt),
+                     height = 0.0) +
+      theme_bw() +
+      facet_grid(.data$condition_3 ~ .data$condition_2) +
+      scale_colour_discrete(name = conditions) +
+      labs(x = "Mean Response Time (ms)",
+           y = "Proportion Accuracy") +
+      theme(text = element_text(size = 18)) +
+      theme(legend.position = "bottom")
+
+    # remove the conditions tag
+    id_averaged_data <- id_averaged_data |> select(-.data$condition_1,
+                                                   -.data$condition_2,
+                                                   -.data$condition_3)
+
+  }
+
+
+  # if conditions greater than three, plot will not be returned
+  if(length(conditions) > 3){
+    message("Number of conditions is greater than three. Plot will not
+            be returned.")
+  }
+
+  return_data <- list(
+    id_data = id_data,
+    id_averaged_data = id_averaged_data,
+    plot = plot
+  )
+  plot(plot)
+  return(return_data)
+}
